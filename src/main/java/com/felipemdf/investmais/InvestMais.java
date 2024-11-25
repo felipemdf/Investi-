7a0f775db1f03a65be89bd632d8a4443d243c54c
felipe <f.marinho162001@gmail.com>
2024-11-24 23:56:07 -0300

    feat: criado o projeto e a tela de transações

---
+++ src/main/java/com/felipemdf/investmais/InvestMais.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.felipemdf.investmais;
+
+import com.felipemdf.investmais.ui.InvestMaisFrame;
+import java.awt.EventQueue;
+
+/**
+ *
+ * @author fmari_v4rpu9g
+ */
+public class InvestMais {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            new InvestMaisFrame().setVisible(true);
+        });
+    }
+}
